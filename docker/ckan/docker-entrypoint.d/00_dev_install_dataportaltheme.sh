412ac9238cd6ba8a993a7ebe1ad66c2aa0ebb28a
Angel Ramboi <angel@ramboi.com>
2021-04-15 18:58:19 +0300

    Additional extensions support and custom data-portal theme

--- docker/ckan/docker-entrypoint.d/00_dev_install_dataportaltheme.sh
+++ docker/ckan/docker-entrypoint.d/00_dev_install_dataportaltheme.sh
@@ -1,13 +1,9 @@
 #!/bin/bash -eux
 
 if [[ $ENV = dev ]] ; then
-
   pip install -r /opt/dataportaltheme/dev-requirements.txt
   pip install -e /opt/dataportaltheme
-
+  pip install -e /opt/datarequests
   sed -i 's/debug = false/debug = true/g' /srv/app/production.ini
-
-  export CKAN__PLUGINS="envvars image_view text_view recline_view datastore xloader resource_proxy stats recline_graph_view webpage_view recline_map_view geo_view geojson_view spatial_metadata spatial_query dataportaltheme"
   ckan config-tool ${APP_DIR}/production.ini "ckan.plugins = ${CKAN__PLUGINS}"
-
 fi
